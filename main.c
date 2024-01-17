26167b36b4f6a6b832acdf98f5984fc4e6b0c7d5
MatasKvn <matas.kvainauskas@protonmail.com>
2024-01-17 21:37:29 +0200

    Defined ArrayList & implemented creation

--- main.c
+++ main.c
@@ -3,9 +3,35 @@
 #include <string.h>
 #include "lib/testlib.h"
 
+#define ARRAYLIST_INITIALCAPACITY 8
+
+typedef struct ArrayList
+{
+    void* elements;
+    size_t elementSize;
+    unsigned int length;
+    unsigned int capacity;
+} ArrayList;
+
+ArrayList* ArrayList_Create(size_t elementSize);
+
 int main(int argc, char** argv)
 {
-    printf("Hello world!\n");
-    testFunc();
+    ArrayList* intList = ArrayList_Create(sizeof(int));
+
     return 0;
 }
+
+
+
+ArrayList* ArrayList_Create(size_t elementSize)
+{
+    ArrayList* list = malloc(sizeof(ArrayList));
+    list->elementSize = elementSize;
+    list->capacity = ARRAYLIST_INITIALCAPACITY;
+    list->length = 0;
+    list->elements = malloc(ARRAYLIST_INITIALCAPACITY * elementSize);
+
+    return list;
+}
+
